bcf5e9387b090a35b0c646c563a735e742c47acc
jshxwh <joshpinoygamer22@gmail.com>
2023-02-21 13:13:21 +0800

    not working category

--- newlecfrontend/src/components/Home.js
+++ newlecfrontend/src/components/Home.js
@@ -81,59 +81,60 @@ const Home = ({ match }) => {
                         ))}
                     </div> */}
             <div className="row">
-              <Fragment>
-                <div className="col-6 col-md-3 mt-5 mb-5">
-                  <div className="px-5">
-                    <Range
-                      marks={{
-                        1: `$1`,
-                        1000: `$1000`,
-                      }}
-                      min={1}
-                      max={1000}
-                      defaultValue={[1, 1000]}
-                      tipFormatter={(value) => `$${value}`}
-                      tipProps={{
-                        placement: "top",
-                        visible: true,
-                      }}
-                      value={price}
-                      onChange={(price) => setPrice(price)}
-                    />
-                    <hr className="my-5" />
-                    <div className="mt-5">
-                      <h4 className="mb-3">Categories</h4>
-                      <ul className="pl-0">
-                        {categories.map((category) => (
-                          <li
-                            style={{
-                              cursor: "pointer",
-                              listStyleType: "none",
-                            }}
-                            key={category}
-                            onClick={() => setCategory(category)}
-                          >
-                            {category}
-                          </li>
-                        ))}
-                      </ul>
+              {keyword ? (
+                <Fragment>
+                  <div className="col-6 col-md-3 mt-5 mb-5">
+                    <div className="px-5">
+                      <Range
+                        marks={{
+                          1: `$1`,
+                          1000: `$1000`,
+                        }}
+                        min={1}
+                        max={1000}
+                        defaultValue={[1, 1000]}
+                        tipFormatter={(value) => `$${value}`}
+                        tipProps={{
+                          placement: "top",
+                          visible: true,
+                        }}
+                        value={price}
+                        onChange={(price) => setPrice(price)}
+                      />
+                      <hr className="my-5" />
+                      <div className="mt-5">
+                        <h4 className="mb-3">Categories</h4>
+                        <ul className="pl-0">
+                          {categories.map((category) => (
+                            <li
+                              style={{
+                                cursor: "pointer",
+                                listStyleType: "none",
+                              }}
+                              key={category}
+                              onClick={() => setCategory(category)}
+                            >
+                              {category}
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
                     </div>
                   </div>
-                </div>
 
-                <div className="col-6 col-md-9">
-                  <div className="row">
-                    {products.map((product) => (
-                      <Product key={product._id} product={product} col={4} />
-                    ))}
+                  <div className="col-6 col-md-9">
+                    <div className="row">
+                      {products.map((product) => (
+                        <Product key={product._id} product={product} col={4} />
+                      ))}
+                    </div>
                   </div>
-                </div>
-              </Fragment>
-              {/* (
+                </Fragment>
+              ) : (
                 products.map((product) => (
                   <Product key={product._id} product={product} col={3} />
                 ))
-              )} */}
+              )}
             </div>
           </section>
           {resPerPage <= count && (
