b8b4ff1167b206ad3aad24a8dadcec0854c2f042
jshxwh <joshpinoygamer22@gmail.com>
2023-02-23 10:22:25 +0800

    working category  - feb 23

--- newlecfrontend/src/actions/productActions.js
+++ newlecfrontend/src/actions/productActions.js
@@ -11,14 +11,18 @@ import {
 } from "../constants/productConstants";
 
 export const getProducts =
-  (keyword = "", currentPage = 1, price, category) =>
+  (keyword = "", currentPage = 1, price, category = "") =>
   async (dispatch) => {
     try {
       dispatch({
         type: ALL_PRODUCTS_REQUEST,
       });
-      let link = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[lte]=${price[1]}&price[gte]=${price[0]}&category=${category}`;
+      let link = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[lte]=${price[1]}&price[gte]=${price[0]}`;
       // let link = `/api/v1/products`;
+
+      if (category) {
+        link = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[lte]=${price[1]}&price[gte]=${price[0]}&category=${category}`;
+      }
 
       const { data } = await axios.get(link);
       console.log(data);
