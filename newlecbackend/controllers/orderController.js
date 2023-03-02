511703379b951570529711c773000af32b0e8f7e
jshxwh <joshpinoygamer22@gmail.com>
2023-03-02 11:26:27 +0800

    march 2  - working orders

--- newlecbackend/controllers/orderController.js
+++ newlecbackend/controllers/orderController.js
@@ -66,3 +66,77 @@ exports.getSingleOrder = async (req, res, next) => {
     order,
   });
 };
+
+exports.myOrders = async (req, res, next) => {
+  const orders = await Order.find({ user: req.user._id });
+
+  // console.log(req.user)
+
+  res.status(200).json({
+    success: true,
+
+    orders,
+  });
+};
+
+exports.allOrders = async (req, res, next) => {
+  const orders = await Order.find();
+
+  // console.log(orders)
+
+  let totalAmount = 0;
+
+  orders.forEach((order) => {
+    totalAmount += order.totalPrice;
+  });
+
+  res.status(200).json({
+    success: true,
+
+    totalAmount,
+
+    orders,
+  });
+};
+
+exports.updateOrder = async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order.orderStatus === "Delivered") {
+    return next(new ErrorHandler("You have already delivered this order", 400));
+  }
+
+  order.orderItems.forEach(async (item) => {
+    await updateStock(item.product, item.quantity);
+  });
+
+  (order.orderStatus = req.body.status), (order.deliveredAt = Date.now());
+
+  await order.save();
+
+  res.status(200).json({
+    success: true,
+  });
+};
+
+async function updateStock(id, quantity) {
+  const product = await Product.findById(id);
+
+  product.stock = product.stock - quantity;
+
+  await product.save({ validateBeforeSave: false });
+}
+
+exports.deleteOrder = async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    return next(new ErrorHandler("No Order found with this ID", 404));
+  }
+
+  await order.remove();
+
+  res.status(200).json({
+    success: true,
+  });
+};
--- newlecbackend/routes/order.js
+++ newlecbackend/routes/order.js
@@ -19,5 +19,17 @@ const {
 const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
 
 router.route("/order/new").post(isAuthenticatedUser, newOrder);
+router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
+router.route("/orders/me").get(isAuthenticatedUser, myOrders);
+router
+  .route("/admin/orders/")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), allOrders);
+router
+  .route("/admin/order/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateOrder);
+
+router
+  .route("/admin/order/:id")
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteOrder);
 
 module.exports = router;
